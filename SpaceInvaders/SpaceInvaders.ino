a7a341620e668bba59be5d5346e783d7fe4dca79
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-08-28 15:32:57 +1000

    Removed extraneous snake code, added some enemy movement

--- SpaceInvaders/SpaceInvaders.ino
+++ SpaceInvaders/SpaceInvaders.ino
@@ -22,7 +22,7 @@
  see also http://www.tigoe.net/pcomp/code/category/arduinowiring/514 for more
  */
 
-#define MAX 64
+#define INIT_ENEMIES 2
 
 // 2-dimensional array of row pin numbers:
 const int row[8] = {
@@ -34,94 +34,29 @@ const int col[8] = {
   6, 11, 10, 3, 17, 4, 8, 9
 };
 
-// 2-dimensional array of pixels:
-int pixels[8][8];
-
-const int uqcsPixels[8][8] = {
-  {0, 0, 1, 0, 0, 1, 0, 0},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-  {1, 0, 0, 0, 0, 0, 0, 1},
-  {1, 0, 0, 0, 0, 0, 0, 1},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 0, 1, 0, 0, 1, 0, 0}
-};
-
-const int partyParrot[8][8] = {
-  {0, 0, 1, 0, 1, 0, 0, 0},
-  {0, 1, 1, 1, 1, 1, 0, 0},
-  {0, 1, 0, 1, 0, 1, 0, 0},
-  {1, 1, 1, 1, 1, 1, 1, 0},
-  {1, 1, 1, 0, 1, 1, 1, 0},
-  {0, 1, 1, 0, 1, 1, 1, 0},
-  {1, 1, 1, 1, 1, 1, 1, 1},
-  {1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-const int partyParrot2[8][8] = {
-  {0, 0, 0, 1, 0, 1, 0, 0},
-  {0, 0, 1, 1, 1, 1, 0, 0},
-  {0, 1, 1, 0, 1, 0, 1, 0},
-  {0, 1, 1, 1, 1, 1, 1, 0},
-  {0, 1, 1, 0, 1, 1, 1, 0},
-  {0, 1, 1, 0, 1, 1, 1, 0},
-  {1, 1, 1, 1, 1, 1, 1, 1},
-  {1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-int rachcatch[8][8] = {
-  {0, 0, 0, 0, 1, 0, 0, 0},
-  {0, 1, 0, 1, 1, 1, 0, 0},
-  {0, 0, 1, 1, 1, 1, 1, 0},
-  {0, 1, 0, 0, 1, 1, 0, 0},
-  {0, 1, 0, 0, 0, 1, 1, 0},
-  {0, 0, 1, 1, 1, 0, 0, 0},
-  {0, 0, 0, 1, 0, 0, 0, 0},
-  {0, 0, 1, 1, 1, 0, 0, 0}  
-};
-
-int fiestarachcatch[8][8] = {
-  {0, 1, 0, 0, 0, 0, 0, 0},
-  {0, 1, 0, 1, 1, 1, 0, 0},
-  {0, 0, 1, 1, 1, 1, 0, 0},
-  {0, 1, 0, 0, 1, 1, 0, 1},
-  {0, 1, 0, 0, 0, 1, 1, 0},
-  {0, 0, 1, 1, 1, 0, 0, 0},
-  {0, 0, 0, 1, 0, 0, 0, 0},
-  {0, 0, 1, 1, 1, 0, 0, 0}  
-};
-
 struct Coord {
   int row;
   int col;
 };
 
-struct Coord board[MAX];
+// player data
+struct Coord player = {.row = 0, .col = 4};
 
-// snek data
-int x = 5;
-int y = 5;
-int snekSize = 0;
-char direction = 'N';
-
-int foodR = -1;
-int foodC = -1;
-int row_ = 0;
-int col_ = 0;
 int splash = 0;
-int fno = 0;
 int tick = 0;
 int flash = 0;
 int fps = 12;
 int tps = 25000;
 
-bool gameOver = false;
+// enemy data
+char direction;
+struct Coord positions[INIT_ENEMIES] = {
+  {.row = 7, .col = 0},
+  {.row = 7, .col = 4}
+};
+int numEnemies = INIT_ENEMIES;
 
-// queue data
-int front = 0;
-int rear = -1;
-// queue size is snekSize
+bool gameOver = false;
 
 void setup() {
   // initialize the I/O pins as outputs
@@ -134,115 +69,84 @@ void setup() {
     // the LEDS are off:
     digitalWrite(col[thisPin], HIGH);
   }
-
-  // initialize the pixel matrix:
-  for (int x = 0; x < 8; x++) {
-    for (int y = 0; y < 8; y++) {
-      pixels[x][y] = HIGH;
-    }
-  }
-  for (int r = 0; r < 8; r++) {
-    for (int c = 0; c < 8; c++) {
-      if (uqcsPixels[r][c]) {
-        draw_pixel(r, c);
-      }
-    }
-  }
-  push({.row = 5, .col = 5});
+  direction = 'E';
   delay(1000);
 }
 
 void loop() {
   // draw the screen:
-  // refreshScreen();
   clear_screen();
-  int analogX = analogRead(0);
-  int analogY = analogRead(1);
-  if (analogX > 1024*3/4) {
-    direction = 'E';
-  } else if (analogX < 1024/4) {
-    direction = 'W';
-  } else if (analogY > 1024*3/4) {
-    direction = 'N';
-  } else if (analogY < 1024/4) {
-    direction = 'S';
-  }
+  
+//  int analogX = analogRead(0);
+//  int analogY = analogRead(1);
+//  if (analogX > 1024*3/4) {
+//  } else if (analogX < 1024/4) {
+//  } else if (analogY > 1024*3/4) {
+//  } else if (analogY < 1024/4) {
+//  }
+  draw_pixel(0, 0);
   if (tick++ > tps/fps) {
-    move();
+    move_enemy();
     if (gameOver) {
       end_game();
     }
-    spawn_food();
     tick = 0;
   }
-  draw_snek();
-  
-  if (flash++ < tps/32) {
-    draw_pixel(foodR, foodC);
-  } else if (flash == tps/16 - 1) {
-    flash = 0;
-  }
-//  draw_image(fiestarachcatch);
+  draw_player();
+  draw_enemies();
 }
 
-void draw_snek() {
-  int end_ = rear >= front ? rear : MAX - 1;
-  for (int i = front; i <= end_; i++) {
-      draw_pixel(board[i].row, board[i].col);
-  }
-  if (front > rear) {
-    for (int i = 0; i <= rear; i++) {
-      draw_pixel(board[i].row, board[i].col);
-    }
-  }
+void draw_player() {
+  draw_pixel(player.row, player.col);
 }
 
 void end_game() {
   long gameOverTimer = 0;
   while (gameOverTimer < 100000) {
     if (gameOverTimer % 10000 < 5000) {
-      draw_snek();
+      draw_player();
     }
     gameOverTimer++;
   }
   asm volatile(" jmp 0");
 }
 
-void move() {
-  if (direction == 'N') {
-    y += 1;
-  } else if (direction == 'E') {
-    x += 1;
-  } else if (direction == 'S') {
-    y -= 1;
-  } else if (direction == 'W') {
-    x -= 1;
-  }
-  // wrap for the moment
-  y = y == 8 ? 0 : y;
-  x = x == 8 ? 0 : x;
-  y = y == -1 ? 7 : y;
-  x = x == -1 ? 7 : x;
-  
-  struct Coord coord = {.row = y, .col = x};
-  if (find(coord) != -1) {
-    // The snek has collided
-    gameOver = true;
-    return;
-  }
-  push(coord);
-  if (x == foodC && y == foodR) {
-    foodR = -1;
-    foodC = -1;
+void move_enemy() {
+  if (direction == 'E') {
+    if (positions[numEnemies - 1].col == 7) {
+      // Move downwards
+      for (int i = 0; i < numEnemies; i++) {
+        positions[i].row -= 1;
+      }
+      direction = 'W';
+    } else {
+      // Move across
+      for (int i = 0; i < numEnemies; i++) {
+        positions[i].col += 1;
+      }
+    }
   } else {
-    pop();
+    if (positions[0].col == 0) {
+      // Move downwards
+      for (int i = 0; i < numEnemies; i++) {
+        positions[i].row -= 1;
+      }
+      direction = 'E';
+    } else {
+      // Move across
+      for (int i = 0; i < numEnemies; i++) {
+        positions[i].col -= 1;
+      }
+    }
   }
 }
 
-void spawn_food() {
-  if (foodR == -1) {
-    foodR = random(0, 7);
-    foodC = random(0, 7);
+void draw_enemies() {
+  for (int i = 0; i < numEnemies; i++) {
+    draw_pixel(positions[i].row, positions[i].col);
+    draw_pixel(positions[i].row, positions[i].col + 1);
+    draw_pixel(positions[i].row + 1, positions[i].col);
+    draw_pixel(positions[i].row + 1, positions[i].col + 1);
   }
 }
 
@@ -286,52 +190,6 @@ void draw_col(int c) {
     digitalWrite(row[i], LOW);
   }
   digitalWrite(col[c], HIGH);
-}
-
-// Queue functions
-
-struct Coord peek() {
-  return board[front];
-}
-
-bool is_full() {
-  return snekSize == MAX;
-}
-
-void push(struct Coord coord) {
-  if (!is_full()) {
-    if (rear == MAX - 1) {
-      rear = -1;
-    }
-    board[++rear] = coord;
-    snekSize++;
-  }
-}
-
-struct Coord pop() {
-  struct Coord coord = board[front++];
-  if (front == MAX) {
-    front = 0;
-  }
-  snekSize--;
-  return coord;
-}
-
-int find(struct Coord coord) {
-  int end_ = rear >= front ? rear : MAX - 1;
-  for (int i = front; i <= end_; i++) {
-    if (board[i].row == coord.row && board[i].col == coord.col) {
-      return i;
-    }
-  }
-  if (front > rear) {
-    for (int i = 0; i <= rear; i++) {
-      if (board[i].row == coord.row && board[i].col == coord.col) {
-        return i;
-      }
-    }
-  }
-  return -1;
 }
 
 
